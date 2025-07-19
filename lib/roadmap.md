185ab4833f9c3255934de8c7d3427aa62a3cad9e
3shmawi <mohamedashmawy918@gmail.com>
2025-07-19 12:37:51 +0300

    7. Row 8. Column 9. Expanded 10. Spacer 11. Container

---
+++ lib/roadmap.md
@@ -0,0 +1,17 @@
+# Flutter widgets roadmap
+##     StatelessWidget     vs     StatefulWidget
+
+
+# Widgets
+1. MaterialApp
+2. Scaffold
+3. AppBar
+4. Text
+5. Icon
+6. IconButton
+
+7. Row
+8. Column 
+9. Expanded
+10. Spacer
+11. Container
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -6,37 +6,108 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.cyan,
-        foregroundColor: Colors.white,
-        title: Text(
-          "Splash Screen",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w400,
-            color: Colors.cyan[900],
-          ),
-        ),
-        centerTitle: true,
-        leading: Icon(Icons.add_a_photo),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add_a_photo),
-            onPressed: () {
-              // Action for the button
-            },
-          ),
-          IconButton(
-            onPressed: () {
-              print('Notification button pressed');
-            },
-            icon: Icon(
-              Icons.notification_add,
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.cyan,
+          foregroundColor: Colors.white,
+          title: Text(
+            "Splash Screen",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w400,
+              color: Colors.cyan[900],
             ),
           ),
-        ],
-      ),
-    );
+          centerTitle: true,
+          leading: Icon(Icons.add_a_photo),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add_a_photo),
+              onPressed: () {
+                // Action for the button
+              },
+            ),
+            IconButton(
+              onPressed: () {
+                print('Notification button pressed');
+              },
+              icon: Icon(
+                Icons.notification_add,
+              ),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Row(
+            children: [
+              Spacer(),
+              Container(
+                height: double.infinity,
+                width: 10,
+                decoration: BoxDecoration(
+                  color: Colors.cyan,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 10,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 10,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 10,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 10,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 10,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 10,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 10,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 10,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 10,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+              Container(
+                height: double.infinity,
+                width: 10,
+                color: Colors.cyan,
+              ),
+              Spacer(),
+            ],
+          ),
+        ));
   }
 }
