84125c1e717bacae3f6b4d929fc8e17f87297094
3shmawi <mohamedashmawy918@gmail.com>
2025-09-06 21:16:32 +0300

    init dog image api

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -65,6 +67,8 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+
+      log('Counter incremented to $_counter');
     });
   }
 
